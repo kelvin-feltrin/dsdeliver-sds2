e729e18f903964f6c54a857d248086731414c106
klfeltrin <kelvin.delucca@gmail.com>
2021-01-10 18:50:45 -0300

    Add Orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -5,6 +5,7 @@ import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fo
 import AppLoading from 'expo-app-loading';
 import Header from './src/Header';
 import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -19,8 +20,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home />
+      <Routes/>
     </View>
   );
 }
