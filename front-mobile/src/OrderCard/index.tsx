a5703def3b82cd93bfe388e05d3ec4400dcc206b
klfeltrin <kelvin.delucca@gmail.com>
2021-01-13 22:26:17 -0300

    formatPrice bug fixed

--- front-mobile/src/OrderCard/index.tsx
+++ front-mobile/src/OrderCard/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import { Order } from '../types';
 import dayjs from 'dayjs';
 import 'dayjs/locale/pt-br';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import "intl";
+import "intl/locale-data/jsonp/pt-BR.js";
+
+if (Platform.OS === "android") {
+    if (typeof (Intl as any).disableRegExpRestore === "function") {
+        (Intl as any).disableRegExpRestore();
+    }
+}
 
 dayjs.locale('pt-br');
 dayjs.extend(relativeTime);
@@ -16,13 +24,21 @@ function dateFromNow(date: string) {
     return dayjs(date).fromNow();
 }
 
+export function formatPrice(price: number){
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    });
+
+    return formatter.format(price);
+}
+
 export default function OrderCard({ order }: Props) {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.orderName}>Pedido {order.id}</Text>
-                {/* To-do: fix function formatPrice */}
-                <Text style={styles.orderPrice}>{order.total}</Text>
+                <Text style={styles.orderPrice}>{formatPrice(order.total)}</Text>
             </View>
             <Text style={styles.text}>{dateFromNow(order.moment)}</Text>
             <View style={styles.productsList}>
